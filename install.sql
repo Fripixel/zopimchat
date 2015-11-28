2abae469e8e155beffacd8562e2352539ed6d7cc
Felipe <felipeventurino@hotmail.com>
2015-11-28 13:39:40 -0200

    add chat to all layouts

---
+++ install.sql
@@ -0,0 +1,13 @@
+INSERT oc_layout_module SET layout_id = 1, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 2, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 3, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 4, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 5, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 6, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 7, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 8, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 9, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 10, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 11, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 12, POSITION = 'content_bottom', CODE = 'zopimchat';
+INSERT oc_layout_module SET layout_id = 13, POSITION = 'content_bottom', CODE = 'zopimchat';
